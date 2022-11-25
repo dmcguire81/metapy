2db6654ad1ca4a244c389d6f4e25b68cfd2be760
Jose Cols <josecolsg@gmail.com>
2022-11-24 23:34:44 -0800

    Test Py311 multibuild

--- travis/install_osx.sh
+++ travis/install_osx.sh
@@ -9,7 +9,7 @@ python -V
 brew update
 brew outdated cmake || brew upgrade cmake
 git clone --recursive https://github.com/MacPython/terryfy
-git clone https://github.com/matthew-brett/multibuild
+git clone https://github.com/josecols/multibuild
 source multibuild/osx_utils.sh
 get_macpython_environment $VERSION venv
 pip install wheel
